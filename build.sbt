c3e4bae31101e64856da174934802ad387aa1a34
= <=>
2016-03-29 17:59:26 -0300

    recebendo response e iniciando parse

--- build.sbt
+++ build.sbt
@@ -4,7 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-
+libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "2.2.4" % "test",
+                            "org.apache.httpcomponents" % "httpclient" % "4.0-alpha4",
+                            "org.json4s" % "json4s-native_2.11" % "3.3.0")
 
 fork in run := true
--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,12 +1,48 @@
+import org.apache.http.impl.client.DefaultHttpClient
+import java.io._
+import org.apache.http.HttpEntity
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.DefaultHttpClient
+import scala.collection.mutable.StringBuilder
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
 object Hello {
+
+  implicit val formats = DefaultFormats
+
+  case class Person(name: String, height: String)
+
   def main(args: Array[String]): Unit = {
     def GetUrlContent = {
-
       val url = "http://swapi.co/api/people/1"
+
       val result = scala.io.Source.fromURL(url).mkString
       println(result)
     }
 
-    GetUrlContent
+    def GetRestContent : String = {
+      val url = "http://swapi.co/api/people/1"
+
+      val httpClient = new DefaultHttpClient
+      val httpResponse = httpClient.execute(new HttpGet(url))
+      val entity = httpResponse.getEntity()
+      var content = ""
+      if (entity != null) {
+        val inputStream = entity.getContent()
+        content = io.Source.fromInputStream(inputStream).getLines.mkString
+        inputStream.close
+      }
+      httpClient.getConnectionManager().shutdown()
+      
+      println(parse(content).extract[Person])
+
+      return content
+    }
+
+    println(GetRestContent)
   }
 }
+
