44bec0323e785bb5dadf7bd4b121dbce3fab6611
= <=>
2016-04-26 17:19:11 -0300

    Rescue service

--- src/main/scala/RescueController.scala
+++ src/main/scala/RescueController.scala
@@ -13,12 +13,7 @@ import org.json4s.native.JsonMethods._
 object RescueController {
 
   def Rescue : Map[String, List[PeopleController.Person]] = {
-    Map[String, List[PeopleController.Person]] map = PeopleController.GetPeople.groupBy(_.species(0))
-    ListMap(map.toSeq.sortWith(_._birth_year > _._birth_year):_*)
-    //map.sortBy((m) -> m._2.birth_year)
-    //Map("foo"->3, "raise"->1, "the"->2, "bar"->4).toList sortBy {_._2}
-
-    //map.reduceLeft(max)
+    PeopleController.GetPeople.groupBy(_.species(0))
   }
 
   def max(p1: PeopleController.Person, p2: PeopleController.Person): PeopleController.Person = {
