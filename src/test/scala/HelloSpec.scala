7d6f0aca338e507891a5d2d3587c2c4942c320ab
= <=>
2016-04-27 10:49:39 -0300

    Inciando TDD

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,7 +1,18 @@
 import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
+
+
   "Hello" should "have tests" in {
     true should be === true
   }
+
+  "SaveElder" should "return 1" in {
+      RescueController.SaveElder(mockElder) should be === mockElder
+  }
+
+  def mockElder :  Map[String, List[PeopleController.Person]] = {
+    Map("1" -> List(PeopleController.Person("name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender", List("specie"), "created", "edited", "url")))
+  }
+
 }
