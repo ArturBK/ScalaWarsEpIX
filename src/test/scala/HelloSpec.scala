a060c181e287746d06e41771a6be17a08476eef9
= <=>
2016-04-27 11:05:28 -0300

    Inciando TDD

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -11,8 +11,15 @@ class HelloSpec extends FlatSpec with Matchers {
       RescueController.SaveElder(mockElder) should be === mockElder
   }
 
-  def mockElder :  Map[String, List[PeopleController.Person]] = {
-    Map("1" -> List(PeopleController.Person("name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender", List("specie"), "created", "edited", "url")))
+  "SaveAll" should "return 1" in {
+    RescueController.SaveElder(mockElder) should be === mockElder
   }
 
+  "Rescue" should "return 1" in {
+    RescueController.SaveElder(mockElder) should be === mockElder
+  }
+
+  def mockElder :  Map[String, List[PeopleController.Person]] = {
+    Map("1" -> List(PeopleController.Person("name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender", List("specie"), "created", "edited", "url")),List(PeopleController.Person("name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender", List("specie"), "created", "edited", "url")))
+  }
 }
